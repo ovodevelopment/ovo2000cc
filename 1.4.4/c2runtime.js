e675298f5301a956af81f7629816ef7c17b70b12
packjack <83289186+Packjackisback@users.noreply.github.com>
2023-12-28 21:49:17 -0600

    Update c2runtime.js

--- 1.4.4/c2runtime.js
+++ 1.4.4/c2runtime.js
@@ -40039,7 +40039,7 @@ cr.behaviors.Platform = function(runtime)
 	{
 		this.maxspeed = maxspeed;
 		if (this.maxspeed < 0)
-			this.maxspeed = 0;
+			this.maxspeed = 2000;
 	};
 	Acts.prototype.SetAcceleration = function (acc)
 	{
